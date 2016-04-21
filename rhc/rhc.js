792c6841bb4e251685b4fcd69c79437454bbfc9c
Noam Lerner <noaml1295@gmail.com>
2016-04-20 22:20:30 -0400

    evalution began

--- rhc/rhc.js
+++ rhc/rhc.js
@@ -1,13 +1,31 @@
 var schedEdit = require('./schedEdit');
-function evaluate(sched){
-	console.log(sched)
+var prefs = {
+	'Low':1,
+	'Med':2,
+	'High':3,
+	'Must':5,
+	'Never':-5
+};
+function evaluate(schedPrefs, sched){
+	/*
+		Need to address:
+			class-group priority
+			total credit horus/classes
+			min/max from groups
+			calendar times
+	*/
+	var score = 0;
+	sched.forEach(function(course){
+		score += prefs[course.priority]*
+			prefs[schedPrefs.groups[course.groupName].priority];
+	});	
 }
-function schedClimber(schedPref,cal){
+
+function schedClimber(schedPrefs,cal){
 	sched = [];
-	sched = schedEdit.addRandCourse(schedPref,sched);
-	evaluate(sched)
-	sched = schedEdit.changeRandCourse(schedPref,sched)
-	evaluate(sched)
+	sched = schedEdit.addRandCourse(schedPrefs,sched);
+	evaluate(schedPrefs, sched);
+	console.log(cal)
 }
 module.exports = function(preferences){
 	schedClimber(preferences.schedule, preferences.cal);
